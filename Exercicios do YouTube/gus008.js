88ebad2f11393fa3f2178f278c5b1c3ac8eec81e
alvaroags <alvarogsilva.neto@gmail.com>
2022-04-21 22:38:50 -0300

    curso em video ex008
    atividade somente em JS sistema de multa

---
+++ Exercicios do YouTube/gus008.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 60){
+    console.log(`Voce esta acima do limite de velocidade. MULTADO!`)
+}
+console.log(`Digira sempre com cuidado`)
